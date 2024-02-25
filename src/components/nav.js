8564f52d01fe025e711da788fd41d29e38c33965
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-02-26 02:54:53 +0500

    nav and its css

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,19 +1,15 @@
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { useState } from "react";
+
 
 const Navbar = () => {
 
-    const [active, setActive ] = useState('');
-    const updateActiveLink = (e) => {
-        setActive(e);
-    }
     return(
         <div className="container">
-            <h1><a href="home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('home')}>Portfolio</a></h1>
+            <h1><a href="home">Portfolio</a></h1>
             <div className="page-links">
-                <a href="#" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('skills')}>Skills</a>
+                <a href="#">Skills</a>
                 <a href="#">Projects</a>
                 <a href="#">Contact</a>
             </div>
